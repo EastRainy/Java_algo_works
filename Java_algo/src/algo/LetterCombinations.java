61766d0b15f99ea1c0d33ea872e380ca70749049
EastRainy <48935971+EastRainy@users.noreply.github.com>
2022-05-09 13:33:00 +0900

    LetterCombination prob code refactoring

--- Java_algo/src/algo/LetterCombinations.java
+++ Java_algo/src/algo/LetterCombinations.java
@@ -10,21 +10,15 @@ public class LetterCombinations {
         TreeNode leftNode, middleNode, rightNode, exepNode;
 
         public TreeNode(String digits, int w){
-
             if(digits.length()>0){
                 crr = (char)(keypad(digits.charAt(0))+w);
-
                 if(digits.length()>1){
                     String tmp = digits.substring(1);
                     leftNode = new TreeNode(tmp, 0);
                     middleNode = new TreeNode(tmp, 1);
                     rightNode = new TreeNode(tmp, 2);
-                    if(tmp.charAt(0)=='7'||(tmp.charAt(0)=='9')){
-                        exepNode = new TreeNode(tmp, 3);
-                    }
-                    else{
-                        exepNode = null;
-                    }
+                    if(tmp.charAt(0)=='7'||(tmp.charAt(0)=='9')){exepNode = new TreeNode(tmp, 3);}
+                    else{exepNode = null;}
                 }
                 else{
                     leftNode = null;
@@ -34,39 +28,27 @@ public class LetterCombinations {
                 }
             }
         }
+        public void addListAll(char c, List<String> input, List<String> ans){
+            for(int i=0; i<input.size(); i++){
+                ans.add(crr+(input.get(i)).toString());
+            }
+        }
         public List<String> returnString(){
-
 
             List<String> ans = new LinkedList<String>();
 
             if(leftNode!=null){
-                List<String> ansL = leftNode.returnString();
-                for(int i=0; i<ansL.size(); i++){
-                    ans.add(crr+(ansL.get(i)).toString());
-                }
-                List<String> ansM = middleNode.returnString();
-                for(int i=0; i<ansM.size(); i++){
-                    ans.add(crr+(ansM.get(i)).toString());
-                }
-                List<String> ansR = rightNode.returnString();
-                for(int i=0; i<ansR.size(); i++){
-                    ans.add(crr+(ansR.get(i)).toString());
-                }
+                addListAll(crr, leftNode.returnString(), ans);
+                addListAll(crr, middleNode.returnString(), ans);
+                addListAll(crr, rightNode.returnString(), ans);
                 if(exepNode!=null){
-                    List<String> ansE = exepNode.returnString();
-                    for(int i=0; i<ansE.size(); i++){
-                        ans.add(crr+(ansE.get(i)).toString());
-                    }
+                    addListAll(crr, exepNode.returnString(), ans);
                 }
-
             }
-            else{
-                ans.add(""+crr);
-            }
+            else{ans.add(""+crr);}
             
             return ans;
-
-        }        
+        }
     }
     public char keypad(char c){
         int input = Integer.parseInt(""+c);
@@ -74,10 +56,7 @@ public class LetterCombinations {
         if(input>7){ans++;}
         return ans;
     }
-
-
     public List<String> letterCombinations(String digits){
-
 
         TreeNode[] root = new TreeNode[4];
         List<String> ans = new LinkedList<String>();
@@ -86,15 +65,9 @@ public class LetterCombinations {
             return ans;
         }
         else{
-            for(int i=0; i<3; i++){
-                root[i] = new TreeNode(digits, i);
-            }
-            if(digits.charAt(0)=='7'||digits.charAt(0)=='9'){
-                root[3] = new TreeNode(digits, 3);
-            }
-            else{
-                root[3]=null;
-            }
+            for(int i=0; i<3; i++){root[i] = new TreeNode(digits, i);}
+            if(digits.charAt(0)=='7'||digits.charAt(0)=='9'){root[3] = new TreeNode(digits, 3);}
+            else{root[3]=null;}
 
             ans.addAll(root[0].returnString());
             ans.addAll(root[1].returnString());
@@ -102,7 +75,6 @@ public class LetterCombinations {
             if(root[3]!=null){
                 ans.addAll(root[3].returnString());
             }
-    
             return ans;
         }
     }
